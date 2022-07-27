60f66c16912f80dfe4cf5b147e5b881ad5ff54c7
DzKalinin <dmitrykalinin94@gmail.com>
2022-07-27 22:22:20 +0400

    update time zone to UTC

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 ENV["RACK_ENV"] ||= 'development'
 require './dependencies'
 
-Time.zone = ActiveSupport::TimeZone['Eastern Time (US & Canada)']
-Time.zone_default = ActiveSupport::TimeZone['Eastern Time (US & Canada)']
+Time.zone = ActiveSupport::TimeZone['UTC']
+Time.zone_default = ActiveSupport::TimeZone['UTC']
 
 Firestore = Google::Cloud::Firestore.new(
   project_id: Settings.app[:google_project_id],
